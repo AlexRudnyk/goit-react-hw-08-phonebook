119935d5027d17763b7a898f946822efccdd1877
AlexRudnyk <alexander.rudnyk@ukr.net>
2022-12-20 09:14:08 +0200

    Added LoginView

--- src/components/appBar/AppBar.jsx
+++ src/components/appBar/AppBar.jsx
@@ -2,12 +2,14 @@ import { Navigation, AppWrapper, AppBarLink, AppTitle } from './AppBar.styled';
 
 export const AppBar = () => {
   return (
-    <AppWrapper>
-      <AppTitle>Phonebook</AppTitle>
-      <Navigation>
-        <AppBarLink>Registration</AppBarLink>
-        <AppBarLink>Login</AppBarLink>
-      </Navigation>
-    </AppWrapper>
+    <>
+      <AppWrapper>
+        <AppTitle>Phonebook</AppTitle>
+        <Navigation>
+          <AppBarLink>Registration</AppBarLink>
+          <AppBarLink>Login</AppBarLink>
+        </Navigation>
+      </AppWrapper>
+    </>
   );
 };
